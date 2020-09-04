436fd7d23a3b9d12e6a0abfa97aaf9ec75001edf
sapphi-red <green@sapphi.red>
2020-09-04 16:38:51 +0900

    f

--- main.go
+++ main.go
@@ -3,6 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello")
-	fmt.Println("hello")
+	i := 0
+	i = 2
+	i = 4
+	if true {
+		fmt.Println(i)
+	}
+
+	slice := []string{}
+	fmt.Println(slice[len(slice)])
+
+	for i := 0; i < 10; i-- {
+		fmt.Println(i)
+	}
+
+	slice2 := []string{}
+	fmt.Println(slice2[len(slice2)])
+
+	return
+
+	fmt.Println("unreach")
+}
+
+func f() {
+	fmt.Println("un")
 }

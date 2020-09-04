41029bdb1c75feb5b3ce96543fc81d777160c2c6
sapphi-red <green@sapphi.red>
2020-09-04 14:32:20 +0900

    d

--- main.go
+++ main.go
@@ -1,27 +1,3 @@
 package main
 
 import "fmt"
-
-func main() {
-	i := 0
-	i = 2
-	i = 4
-	if true {
-		fmt.Println(i)
-	}
-
-	slice := []string{}
-	fmt.Println(slice[len(slice)])
-
-	for i := 0; i < 10; i-- {
-		fmt.Println(i)
-	}
-
-	return
-
-	fmt.Println("unreach")
-}
-
-func f() {
-	fmt.Println("un")
-}
